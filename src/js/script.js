f49fdd2e738501be8b3d0a21af6568efe17930b9
LevaniMigriauli <levanimigriauli16@gmail.com>
2022-03-15 20:38:19 +0400

    JS done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
 "use strict";
+
+// mobile navigation
+const btnNavEl = document.querySelector(".button-mobile-nav");
+const navigationEl = document.querySelector(".navigation");
+btnNavEl.addEventListener("click", function () {
+  navigationEl.classList.toggle("nav-open");
+});
+
+// Footer set current year
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
