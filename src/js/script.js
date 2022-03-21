4d645909b0ae4597b867b2a52e6bb6bdbd6af97f
LevaniMigriauli <levanimigriauli16@gmail.com>
2022-03-21 19:05:15 +0400

    adding slider dots

--- src/js/script.js
+++ src/js/script.js
@@ -17,9 +17,38 @@ const slides = document.querySelectorAll(".slide");
 const btnLeft = document.querySelector(".slider__btn--left");
 const btnRight = document.querySelector(".slider__btn--right");
 
-const slider = document.querySelector(".section-hero-slider");
-slider.style.transform = "scale(0.4)";
-slider.style.overflow = "visible";
+let curSlide = 0;
+const maxSlide = slides.length;
 
-slides.forEach((s, i) => (s.style.transform = `translateX(${100 * i}%)`));
-//  0%, 100%, 200%, 300%
+// const slider = document.querySelector(".section-hero-slider");
+// slider.style.transform = "scale(0.4)";
+// slider.style.overflow = "visible";
+
+const goToSlide = function (slide) {
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+  );
+};
+goToSlide(0);
+
+// Next slide
+const nextSlide = function () {
+  if (curSlide === maxSlide - 1) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+  goToSlide(curSlide);
+};
+
+const prevSlide = function () {
+  if (curSlide === 0) {
+    curSlide = maxSlide - 1;
+  } else {
+    curSlide--;
+  }
+  goToSlide(curSlide);
+};
+
+btnRight.addEventListener("click", nextSlide);
+btnLeft.addEventListener("click", prevSlide);
