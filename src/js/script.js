4c8b8c2427794a8ad4051bacead0381fa3b240db
LevaniMigriauli <levanimigriauli16@gmail.com>
2022-03-21 17:54:08 +0400

    adding slider buttons functionality

--- src/js/script.js
+++ src/js/script.js
@@ -11,3 +11,15 @@ btnNavEl.addEventListener("click", function () {
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
+
+// Slider
+const slides = document.querySelectorAll(".slide");
+const btnLeft = document.querySelector(".slider__btn--left");
+const btnRight = document.querySelector(".slider__btn--right");
+
+const slider = document.querySelector(".section-hero-slider");
+slider.style.transform = "scale(0.4)";
+slider.style.overflow = "visible";
+
+slides.forEach((s, i) => (s.style.transform = `translateX(${100 * i}%)`));
+//  0%, 100%, 200%, 300%
